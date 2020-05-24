4bb9062ec1a9bf8579b8906802704e07501c9b87
suzukalight <mail@mkubara.com>
2020-05-24 21:23:11 +0900

    Run graphql-codegen and generate ts files

---
+++ src/client/src/generated/modules.d.ts
@@ -0,0 +1,9 @@
+
+declare module '*/user.graphql' {
+  import { DocumentNode } from 'graphql';
+  const defaultDocument: DocumentNode;
+  export const GetUserName: DocumentNode;
+
+  export default defaultDocument;
+}
+    
